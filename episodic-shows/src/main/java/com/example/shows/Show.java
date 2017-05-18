3b4f27949284d52c2d38bc267761ed5cfd8fa2f5
praneethniu <praneethcbit03@gmail.com>
2017-05-18 11:02:48 -0600

    add Shows flow

--- episodic-shows/src/main/java/com/example/shows/Show.java
+++ episodic-shows/src/main/java/com/example/shows/Show.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name="users")
+@Entity(name="shows")
 public class Show {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- episodic-shows/src/main/java/com/example/shows/ShowsController.java
+++ episodic-shows/src/main/java/com/example/shows/ShowsController.java
@@ -5,26 +5,26 @@ import com.example.shows.ShowRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class ShowsController {
 
-    private ShowRepository
-    showRespository;
+    private ShowRepository showRespository;
 
-    public ShowsController(ShowRepository
-                           showRespository) {
-        this.
-                showRespository=
-        showRespository;
+    public ShowsController(ShowRepository showRespository) {
+        this.showRespository = showRespository;
     }
 
     @GetMapping("/shows")
     public List<Show> getShows() {
         List<Show> shows = new ArrayList();
 
+        showRespository.findAll()
+                .forEach(shows::add);
 
         return shows;
     }
